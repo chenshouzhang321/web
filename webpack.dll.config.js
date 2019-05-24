6138f189b01c777c8af295a30f48fa65ee902c29
shzhchen <shzhchen@abcft.com>
2019-05-24 18:08:30 +0800

    1111

---
+++ webpack.dll.config.js
@@ -0,0 +1,27 @@
+var path = require("path"),
+　　fs = require('fs'),
+　　webpack = require("webpack");
+
+var vendors = [
+　　'vue', 
+　　'vue-router', 
+　　'vuex'
+];
+
+module.exports = {
+　　entry: {
+　　　　vendor: vendors
+　　},
+　　output: {
+　　　　path: path.join(__dirname, './static/js'), // 打包后文件输出的位置
+		filename: '[name].dll.js',
+		library: '[name]_library'
+　　},
+　　plugins: [
+　　　　new webpack.DllPlugin({
+　　　　　　path: path.join(__dirname, "./static/js", "manifest.json"),
+　　　　　　name: "[name]_[hash]",
+　　　　　　context: __dirname
+　　　　})
+　　]
+};
